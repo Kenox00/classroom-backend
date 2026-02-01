7f124d691782d82d56af23f449f48d70ef02ecb5
kenox00 <kwihanganaenock99@gmail.com>
2026-02-01 11:16:01 +0200

    implement database schemas

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+
+export default defineConfig({
+  schema: "./src/db/schema/index.ts",
+  out: "./drizzle",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  }
+});
+
