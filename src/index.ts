d8f68677abc69ab8833f0a8844f4559df8975344
kenox00 <kwihanganaenock99@gmail.com>
2026-02-02 20:31:02 +0200

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import express from 'express';
+import subjectRouter from './routes/subject';
+import cors from 'cors';
 
 const app = express();
 const PORT = 8000;
 
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}))
 // Middleware
 app.use(express.json());
 
 // Routes
+
+app.use('/api/subjects', subjectRouter)
+
 app.get('/', (req, res) => {
   res.json({ message: 'Classroom API Server - Ready to serve!' });
 });
